3a73be7eabfc9ae289f9db9a13cb687312094cde
Fizkry <65403847+Fizkry@users.noreply.github.com>
2025-06-01 06:56:34 +0700

    Add .next/static/chunks/src_app_settings_page_tsx_58c612fc._.js 1748735797994

---
+++ .next/static/chunks/src_app_settings_page_tsx_58c612fc._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_b2418d82._.js",
+  "static/chunks/src_ac36ea6d._.js"
+],
+    source: "dynamic"
+});
