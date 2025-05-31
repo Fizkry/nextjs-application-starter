236e83caa00239e9f0029239f956fa0d887d28a5
Fizkry <65403847+Fizkry@users.noreply.github.com>
2025-06-01 06:57:00 +0700

    Add .next/static/chunks/src_app_planning_page_tsx_58c612fc._.js 1748735823682

---
+++ .next/static/chunks/src_app_planning_page_tsx_58c612fc._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_95626497._.js",
+  "static/chunks/src_693b144d._.js"
+],
+    source: "dynamic"
+});
